0e7a76bab7058fb933745f666addebbf30c1c55a
Dainius Remeikis <dainius.remeikis@gmail.com>
2021-02-01 21:28:51 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2021._02._01_wpf_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void MygtukasSudetis(object sender, RoutedEventArgs e)
+        {
+            var pirmas = int.TryParse(pirmasSkaicius.Text, out int pirmasSk);
+            var antras = int.TryParse(antrasSkaicius.Text, out int antrasSk);
+
+            if (pirmas & antras)
+            {
+                rezultatas.Text = $"{pirmasSk + antrasSk}";
+            }
+            else
+            {
+                rezultatas.Text = "Įveskite skaičius";
+            }
+
+        }
+
+        private void MygtukasAtimtis(object sender, RoutedEventArgs e)
+        {
+            var pirmas = int.TryParse(pirmasSkaicius.Text, out int pirmasSk);
+            var antras = int.TryParse(antrasSkaicius.Text, out int antrasSk);
+
+            if (pirmas & antras)
+            {
+                rezultatas.Text = $"{pirmasSk - antrasSk}";
+            }
+            else
+            {
+                rezultatas.Text = "Įveskite skaičius";
+            }
+
+        }
+        private void MygtukasDalyba(object sender, RoutedEventArgs e)
+        {
+            var pirmas = double.TryParse(pirmasSkaicius.Text, out double pirmasSk);
+            var antras = double.TryParse(antrasSkaicius.Text, out double antrasSk);
+
+            if (pirmas & antras)
+            {
+                rezultatas.Text = $"{pirmasSk / antrasSk}";
+            }
+            else
+            {
+                rezultatas.Text = "Įveskite skaičius";
+            }
+
+        }
+        private void MygtukasDaugyba(object sender, RoutedEventArgs e)
+        {
+            var pirmas = double.TryParse(pirmasSkaicius.Text, out double pirmasSk);
+            var antras = double.TryParse(antrasSkaicius.Text, out double antrasSk);
+
+            if (pirmas & antras)
+            {
+                rezultatas.Text = $"{pirmasSk * antrasSk}";
+            }
+            else
+            {
+                rezultatas.Text = "Įveskite skaičius";
+            }
+
+        }
+    }
+}
